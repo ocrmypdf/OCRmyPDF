2b2637fbc3f43710a0ea2d155c7aea8cf6898601
fritz-hh <julien.pfefferkorn@googlemail.com>
2013-12-30 18:21:03 +0100

    minor change

--- src/config.sh
+++ src/config.sh
@@ -13,9 +13,9 @@ LOG_ERR="0"				# only error messages
 LOG_WARN="1"				# error messages and warnings
 LOG_INFO="2"				# error messages, warnings and some infos
 LOG_DEBUG="3"				# debug level logging
-SRC="./src"				# location of the source folder (except source of external tools like jhove)
 
 # various paths
+SRC="./src"				# location of the source folder (except source of external tools like jhove)
 OCR_PAGE="$SRC/ocrPage.sh"		# path to the script aimed at OCRing one page
 JHOVE="./jhove/bin/JhoveApp.jar"	# java SW for validating the final PDF/A
 JHOVE_CFG="./jhove/conf/jhove.conf"	# location of the jhove config file
