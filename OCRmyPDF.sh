c6f2eea058e806179be3579fccf12b49a09999cf
James R. Barlow <jim@purplerock.ca>
2016-08-31 11:57:02 -0700

    Reinstate OCRmyPDF.sh with a deprecation warning

---
+++ OCRmyPDF.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+##############################################################################
+# Copyright (c) 2013-14: fritz-hh from Github (https://github.com/fritz-hh)
+##############################################################################
+
+echo "This script is deprecated. Use 'ocrmypdf' instead."
+
+python3 -m ocrmypdf.main "$@"
