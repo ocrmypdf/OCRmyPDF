6821e8eeb2ba317f56ee712fc6363252fb831bec
Sean Whitton <spwhitton@users.noreply.github.com>
2016-11-01 21:56:57 -0700

    disable mathjax sphinx extension (#103)
    Mathjax isn't actually needed for OCRmyPDF's docs, but enabling this
    extension causes the browser to download a copy of mathjax.js from
    cdn.mathjax.org anyway.
    
    I have to disable this for the offline docs bundled with Debian, but
    since you're not using mathjax, it would be nice to have the diff merged
    upstream.

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.mathjax',
+    # 'sphinx.ext.mathjax',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
