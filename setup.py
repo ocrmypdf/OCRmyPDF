9e004c3ec022ae94570a9447b8892ad52b63e014
James R. Barlow <jim@purplerock.ca>
2017-01-03 00:34:26 -0800

    Another attempt at py 3.4/3.5
    Revert to exactly what the previous passing build specified.

--- setup.py
+++ setup.py
@@ -214,8 +214,7 @@ setup(
         "Topic :: Text Processing :: Linguistic",
         ],
     setup_requires=[
-        'setuptools-scm == 1.11.1',  # pinned because upstream keeps breaking things
-        'setuptools-scm-git-archive',
+        'setuptools_scm',
         'cffi>=1.5.0',
         'pytest-runner'
     ],
