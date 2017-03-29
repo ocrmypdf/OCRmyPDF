23227ae763e20120b5921b6fed4eb040d7b081cb
James R. Barlow <jim@purplerock.ca>
2017-03-29 15:41:52 -0700

    Deprecate old files

--- OCRmyPDF.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-##############################################################################
-# Copyright (c) 2013-14: fritz-hh from Github (https://github.com/fritz-hh)
-##############################################################################
-
-echo "This script is deprecated. Use 'ocrmypdf' instead."
-
-python3 -m ocrmypdf.main "$@"
--- ocrmypdf/main.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# © 2015-16 James R. Barlow: github.com/jbarlow83
-
-# This file is now an alias for __main__
-# Consider removing in future releases
-
-from ocrmypdf.__main__ import *
