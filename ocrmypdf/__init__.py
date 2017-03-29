a9f4047a978d7fcc8064ca824948c1939fcc5cd4
James R. Barlow <jim@purplerock.ca>
2017-03-29 15:42:54 -0700

    Remove backward compatible API deprecations from v4.x

--- ocrmypdf/__init__.py
+++ ocrmypdf/__init__.py
@@ -5,11 +5,3 @@ PROGRAM_NAME = 'ocrmypdf'
 VERSION = pkg_resources.get_distribution('ocrmypdf').version
 
 
-# These imports are for v4.x backward compatibility for consumers of ocrmypdf
-# (if any). They are deprecated and will be removed in v5.x.
-from .exec import ghostscript, qpdf, tesseract, unpaper, get_program
-from .exceptions import ExitCode
-from .helpers import page_number, is_iterable_notstr
-
-
-
