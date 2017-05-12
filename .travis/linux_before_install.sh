ff5c38b1f78d2c7dead3357692bcd74a4d4ed7ae
James R. Barlow <jim@purplerock.ca>
2017-05-11 23:52:13 -0700

    Tell Travis to download Tesseract 4.00 from a PPA for testing

--- .travis/linux_before_install.sh
+++ .travis/linux_before_install.sh
@@ -7,16 +7,22 @@ sudo add-apt-repository ppa:heyarje/libav-11 -y
 sudo apt-get update -qq
 sudo apt-get install -y \
 	ghostscript \
-	tesseract-ocr \
-	tesseract-ocr-deu \
-	tesseract-ocr-eng \
-	tesseract-ocr-fra \
 	qpdf \
 	poppler-utils \
 	libavformat56 \
 	libavcodec56 \
 	libavutil54 \
 	libffi-dev
+
+sudo add-apt-repository ppa:alex-p/tesseract-ocr -y
+
+sudo apt-get update
+sudo apt-get autoremove -y
+sudo apt-get install -y --no-install-recommends \
+		tesseract-ocr \
+		tesseract-ocr-eng \
+		tesseract-ocr-fra \
+		tesseract-ocr-deu
 
 pip install --upgrade pip
 mkdir -p packages
