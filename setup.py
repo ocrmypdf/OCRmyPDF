b6d30214fd978718ec33aae1f172dae7b5f2f492
James R. Barlow <jim@purplerock.ca>
2018-05-12 12:35:46 -0700

    PyMuPDF 1.13.4 looks good, use it

--- setup.py
+++ setup.py
@@ -252,7 +252,7 @@ setup(
         'ruffus == 2.6.3',        # pinned - ocrmypdf implements a 2.6.3 workaround
     ],
     extras_require={
-        'fitz': ['PyMuPDF == 1.12.5'],
+        'fitz': ['PyMuPDF == 1.13.4'],
     },
     tests_require=tests_require,
     entry_points={
