1f40a7055408e30628470c724ff694af3d67f7da
James R. Barlow <jim@purplerock.ca>
2018-05-15 21:43:19 -0700

    Use qpdf 8.0.2 backport, force old pytest-timeout to fix build

--- .travis/linux_before_install.sh
+++ .travis/linux_before_install.sh
@@ -12,7 +12,8 @@ sudo apt-get install -y \
 	libavformat56 \
 	libavcodec56 \
 	libavutil54 \
-	libffi-dev
+	libffi-dev \
+	qpdf
 
 sudo add-apt-repository ppa:alex-p/tesseract-ocr -y
 
@@ -28,15 +29,3 @@ pip install --upgrade pip
 mkdir -p packages
 wget -q 'https://www.dropbox.com/s/vaq0kbwi6e6au80/unpaper_6.1-1.deb?raw=1' -O packages/unpaper_6.1-1.deb
 sudo dpkg -i packages/unpaper_6.1-1.deb
-
-if [ ! -f /usr/local/bin/qpdf ]; then
-	export QPDF_RELEASE='https://github.com/qpdf/qpdf/releases/download/release-qpdf-8.0.2/qpdf-8.0.2.tar.gz'
-	mkdir qpdf
-    wget -q $QPDF_RELEASE -O - | tar xz -C qpdf --strip-components=1
-    cd qpdf/
-	export PATH="/usr/local/opt/ccache/libexec:$PATH"
-    ./configure --prefix=/usr
-    make -j 2
-    sudo make install
-    cd ..
-fi
