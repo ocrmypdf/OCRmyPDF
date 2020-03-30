8307832ce9675d3154c0af04bcf9099908e863fb
James R. Barlow <jim@purplerock.ca>
2020-03-29 22:44:16 -0700

    tests: add force OCR to a file with text that Ghostscript doesn't see
    For gs 9.52 support.
    
    Also refactor use of pikepdf.open() to use with blocks.

--- tests/test_graft.py
+++ tests/test_graft.py
@@ -25,25 +25,30 @@ import ocrmypdf
 
 
 def test_no_glyphless_graft(resources, outdir):
-    pdf = pikepdf.open(resources / 'francais.pdf')
-    pdf_aspect = pikepdf.open(resources / 'aspect.pdf')
-    pdf_cmyk = pikepdf.open(resources / 'cmyk.pdf')
-    pdf.pages.extend(pdf_aspect.pages)
-    pdf.pages.extend(pdf_cmyk.pages)
-    pdf.save(outdir / 'test.pdf')
+    with pikepdf.open(resources / 'francais.pdf') as pdf, pikepdf.open(
+        resources / 'aspect.pdf'
+    ) as pdf_aspect, pikepdf.open(resources / 'cmyk.pdf') as pdf_cmyk:
+        pdf.pages.extend(pdf_aspect.pages)
+        pdf.pages.extend(pdf_cmyk.pages)
+        pdf.save(outdir / 'test.pdf')
 
     with patch('ocrmypdf._graft.MAX_REPLACE_PAGES', 2):
         ocrmypdf.ocr(
-            outdir / 'test.pdf', outdir / 'out.pdf', deskew=True, tesseract_timeout=0
+            outdir / 'test.pdf',
+            outdir / 'out.pdf',
+            deskew=True,
+            tesseract_timeout=0,
+            force_ocr=True,
         )
+    # This test needs asserts
 
 
 def test_links(resources, outpdf):
     ocrmypdf.ocr(
         resources / 'link.pdf', outpdf, redo_ocr=True, oversample=200, output_type='pdf'
     )
-    pdf = pikepdf.open(outpdf)
-    p1 = pdf.pages[0]
-    p2 = pdf.pages[1]
-    assert p1.Annots[0].A.D[0].objgen == p2.objgen
-    assert p2.Annots[0].A.D[0].objgen == p1.objgen
+    with pikepdf.open(outpdf) as pdf:
+        p1 = pdf.pages[0]
+        p2 = pdf.pages[1]
+        assert p1.Annots[0].A.D[0].objgen == p2.objgen
+        assert p2.Annots[0].A.D[0].objgen == p1.objgen
