67f99c5bb730805b60bc5cc46af875af44b6926e
James R. Barlow <james@purplerock.ca>
2020-10-27 17:43:22 -0700

    Endorse pdfminer.six 20201018

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ setup(
         'cffi >= 1.9.1',  # must be a setup and install requirement
         'coloredlogs >= 14.0',  # strictly optional
         'img2pdf >= 0.3.0, < 0.5',  # pure Python, so track HEAD closely
-        'pdfminer.six >= 20191110, != 20200720, <= 20200726',
+        'pdfminer.six >= 20191110, != 20200720, <= 20201018',
         'pikepdf >= 1.14.0, < 3',
         'Pillow >= 7.0.0',
         'pluggy >= 0.13.0, < 1.0',
