208657f840a58d8c740f8b0fcece8f9dc03971f7
James R. Barlow <james@purplerock.ca>
2021-09-13 00:56:19 -0700

    pdfa: replace pkg_resources with importlib.resources

--- src/ocrmypdf/pdfa.py
+++ src/ocrmypdf/pdfa.py
@@ -10,15 +10,19 @@ Utilities for PDF/A production and confirmation with Ghostspcript.
 """
 
 import base64
+import importlib.resources
 from pathlib import Path
 from typing import Dict, Iterator, Union
 
 import pikepdf
-import pkg_resources
+import pkg_resources  # deprecated
 
+# Deprecated
 ICC_PROFILE_RELPATH = 'data/sRGB.icc'
-
+# Deprecated
 SRGB_ICC_PROFILE = pkg_resources.resource_filename('ocrmypdf', ICC_PROFILE_RELPATH)
+
+SRGB_ICC_PROFILE_NAME = 'sRGB.icc'
 
 
 def _postscript_objdef(
@@ -97,12 +101,12 @@ def generate_pdfa_ps(target_filename: Path, icc: str = 'sRGB'):
     References:
         Adobe PDFMARK Reference: https://www.adobe.com/content/dam/acom/en/devnet/acrobat/pdfs/pdfmark_reference.pdf
     """
-    if icc == 'sRGB':
-        icc_profile = SRGB_ICC_PROFILE
-    else:
+    if icc != 'sRGB':
         raise NotImplementedError("Only supporting sRGB")
 
-    bytes_icc_profile = Path(icc_profile).read_bytes()
+    bytes_icc_profile = importlib.resources.read_binary(
+        'ocrmypdf.data', SRGB_ICC_PROFILE_NAME
+    )
     ps = '\n'.join(_make_postscript(icc, bytes_icc_profile, 3))
 
     # We should have encoded everything to pure ASCII by this point, and
