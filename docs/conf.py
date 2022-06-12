6cdf68363a403844fbefd0a406e6549eaec3dd1b
James R. Barlow <james@purplerock.ca>
2022-06-12 00:28:55 -0700

    docs: copyright year

--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,7 @@ master_doc = 'index'
 # General information about the project.
 project = 'ocrmypdf'
 copyright = (
-    '2021, James R. Barlow. Licensed under Creative Commons Attribution-ShareAlike 4.0.'
+    '2022, James R. Barlow. Licensed under Creative Commons Attribution-ShareAlike 4.0.'
 )
 author = 'James R. Barlow'
 
